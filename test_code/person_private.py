56c5d20a28efe40222dd9dd672869828f5bc2234
soufal <wyf3510@126.com>
2020-07-07 15:07:07 +0800

    更新面向对象进阶代码

--- test_code/person_private.py
+++ test_code/person_private.py
@@ -46,5 +46,43 @@ def main():
     print(person._gender)
 
 
+class Person_one(object):
+
+    # 限定Person对象只能绑定_name, _age和_gender属性
+    # 使用__slots__装饰器
+    __slots__ = ('_name', '_age', '_gender')
+
+    def __init__(self, name, age):
+        self._name = name
+        self._age = age
+
+    @property
+    def name(self):
+        return self._name
+
+    @property
+    def age(self):
+        return self._age
+
+    @age.setter
+    def age(self, age):
+        self._age = age
+
+    def play(self):
+        if self._age <= 16:
+            print('%s正在玩飞行棋.' % self._name)
+        else:
+            print('%s正在玩斗地主.' % self._name)
+
+
+def main_one():
+    person = Person_one('王大锤', 22)
+    person.play()
+    person._gender = '男'
+    # AttributeError: 'Person' object has no attribute '_is_gay'
+    # person._is_gay = True
+    # print(person._is_gay)
+
+
 if __name__ == '__main__':
-    main()
+    main_one()
