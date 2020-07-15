a54e1a25f861439137bea6967ce2eb48397d12cd
soufal <wyf3510@126.com>
2020-07-15 14:12:20 +0800

    更新抽象类代码

---
+++ test_code/Pet.py
@@ -0,0 +1,43 @@
+from abc import ABCMeta, abstractmethod
+
+
+"""
+我们将Pet类处理成了一个抽象类，所谓抽象类就是不能够创建对象的类，这种类的存在就是专门为了让其他类去继承它。
+Python从语法层面并没有像Java或C#那样提供对抽象类的支持，
+但是我们可以通过abc模块的ABCMeta元类和abstractmethod包装器来达到抽象类的效果，
+如果一个类中存在抽象方法那么这个类就不能够实例化（创建对象）
+"""
+class Pet(object, metaclass=ABCMeta):
+    """宠物"""
+
+    def __init__(self, nickname):
+        self._nickname = nickname
+
+    @abstractmethod
+    def make_voice(self):
+        """发出声音"""
+        pass
+
+
+class Dog(Pet):
+    """狗"""
+
+    def make_voice(self):
+        print('%s: 汪汪汪...' % self._nickname)
+
+
+class Cat(Pet):
+    """猫"""
+
+    def make_voice(self):
+        print('%s: 喵...喵...' % self._nickname)
+
+
+def main():
+    pets = [Dog('旺财'), Cat('凯蒂'), Dog('大黄')]
+    for pet in pets:
+        pet.make_voice()
+
+
+if __name__ == '__main__':
+    main()
