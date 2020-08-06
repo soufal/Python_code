90897c0188091869210b1d962ab30c42866e7d3f
soufal <wyf3510@126.com>
2020-08-06 14:06:45 +0800

    更新“大球吃小球”代码

--- test_code/Ball_Game.py
+++ test_code/Ball_Game.py
@@ -6,7 +6,7 @@
 # @Desc : 使用pygame实现“大球吃小球”游戏
 
 from enum import Enum, unique
-from math import sort
+from math import sqrt
 from random import randint
 
 import pygame
@@ -43,15 +43,64 @@ class Ball(object):
     def __init__(self, x, y, radius, sx, sy, color=Color.RED):
         """
         初始化方法
-        :param x:
-        :param y:
-        :param radius:
-        :param sx:
-        :param sy:
-        :param color:
+        :param x: 当前x坐标
+        :param y: 当前y坐标
+        :param radius: 球半径
+        :param sx: 需要移动的x距离
+        :param sy: 需要移动的y距离
+        :param color: 球颜色
         """
+        self.x = x
+        self.y = y
+        self.radius = radius
+        self.sx = sx
+        self.sy = sy
+        self.color = color
+        self.alive = True
+
+    def move(self, screen):
+        """
+        球的移动
+        :param screen: 球所在的屏幕大小
+        :return:
+        """
+        self.x += self.sx
+        self.y += self.sy
+        if self.x - self.radius <= 0 or \
+            self.x + self.radius >= screen.get_width():
+                self.sx = -self.sx
+        if self.y - self.radius <= 0 or \
+            self.y + self.radius >= screen.get_height():
+                self.sy = -self.sy
+
+    def eat(self, other):
+        """
+        吃其他球
+        :param other:
+        :return:
+        """
+        if self.alive and other.alive and self != other:
+            dx, dy = self.x - other.x, self.y - other.y
+            distance = sqrt(dx ** 2 + dy ** 2)
+            if distance < self.radius + other.radius \
+                and self.radius > other.radius:
+                    other.alive = False
+                    self.radius = self.radius + int(other.radius * 0.146)
+
+    def draw(self, screen):
+        """
+        在窗口上绘制球
+        :param screen:
+        :return:
+        """
+        pygame.draw.circle(screen, self.color,
+                           (self.x, self.y), self.radius, 0)
+
+
 
 def main():
+    # 定义用来装所有球的容器
+    balls = []
     # 初始化导入的pygame中的模块
     pygame.init()
     # 初始化用于显示的窗口并设置窗口尺寸
@@ -59,29 +108,57 @@ def main():
     # 设置当前窗口的标题
     pygame.display.set_caption('大球吃小球')
     # 定义变量来表示小球在屏幕上的位置
-    x, y =50, 50
+    # x, y =50, 50
     running = True
-    # 开启一个事件循环处理发生的时间
+    # 开启一个事件循环处理发生的事件
     while running:
         # 从消息队列中获取事件并对事件进行处理
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
-        # 设置窗口的背景色（颜色是由红绿蓝三原色构成的元组）
-        screen.fill((242, 242, 242))
-        # 绘制一个圆（参数分别是：屏幕，颜色，圆心位置，半径，0表示填充圆）
-        pygame.draw.circle(screen, (255, 0, 0), (x, y), 30, 0)
 
-        # # 通过指定的文件名加载图像
-        # ball_image = pygame.image.load('./')
-        # # 在窗口上渲染图像
-        # screen.blit(ball_image, (50,50))
-
-        # 刷新当前窗口（渲染窗口将绘制的图像呈现出来）
+            # 处理鼠标事件的代码
+            if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+                # 获得点击鼠标的位置
+                x, y = event.pos
+                radius = randint(10, 100)
+                sx, sy = randint(-10, 10), randint(-10, 10)
+                color = Color.random_color()
+                # 在点击鼠标的位置创建一个球（大小、速度和颜色随机）
+                ball = Ball(x, y, radius, sx, sy, color)
+                # 将球添加到列表容器中
+                balls.append(ball)
+        # 设置窗口的背景色
+        screen.fill((255, 255, 255))
+        # 取出容器中的球， 如果没被吃掉就绘制，被吃掉了就移除
+        for ball in balls:
+            if ball.alive:
+                ball.draw(screen)
+            else:
+                balls.remove(ball)
         pygame.display.flip()
-        # 每隔50毫秒就改变小球的位置再刷新窗口
+        # 每隔50毫秒就改变球的位置再刷新窗口
         pygame.time.delay(50)
-        x, y = x + 5, y + 5
+        for ball in balls:
+            ball.move(screen)
+            # 检查球有没有吃到其他的球
+            for other in balls:
+                ball.eat(other)
+        # # 设置窗口的背景色（颜色是由红绿蓝三原色构成的元组）
+        # screen.fill((242, 242, 242))
+        # # 绘制一个圆（参数分别是：屏幕，颜色，圆心位置，半径，0表示填充圆）
+        # pygame.draw.circle(screen, (255, 0, 0), (x, y), 30, 0)
+        #
+        # # # 通过指定的文件名加载图像
+        # # ball_image = pygame.image.load('./')
+        # # # 在窗口上渲染图像
+        # # screen.blit(ball_image, (50,50))
+        #
+        # # 刷新当前窗口（渲染窗口将绘制的图像呈现出来）
+        # pygame.display.flip()
+        # # 每隔50毫秒就改变小球的位置再刷新窗口
+        # pygame.time.delay(50)
+        # x, y = x + 5, y + 5
 
 
 if __name__ == '__main__':
